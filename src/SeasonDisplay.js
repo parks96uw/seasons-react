ab5b29809d18d7d29455056ad93702dd251e8628
Sarah Park <spark4774@gmail.com>
2020-04-28 13:50:09 -0700

    compoment seasondisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import "./SeasonDisplay.css";
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+// configuration object
+const seasonConfig = {
+    summer: {
+        content: "Let's hit the beach",
+        iconName: "sun"
+    },
+    winter: {
+        content: "Brrr it's chilly",
+        iconName: "snowflake"
+    }
 }
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { content, iconName } = seasonConfig[season]
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}></i>
+            <h1>{content}</h1>
+            <i className={`icon-right massive ${iconName} icon`}></i>
+        </div>
+    );
+}
+
+// if user is in nortnern hemisphere and month is between 3-8, they are in summer, else winter
+// if user is in southern hemisphere and month is between 3-8, they are in winter, else summer
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'; // tertiary statement
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -6,22 +6,10 @@ import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
     // we have one state -- the latitude of the user
 
-    // initialize the sate through the constructor
-    // optional -- not required
-    // the very first function that is called within this App instance component
-    // therefore it is the best place to initialize the state
-    constructor(props) {
-        // makes sure that React.Compononent's constructor function gets called
-        super(props);
-
-        // our state object which will contain our relevant data
-        // only time we do direct assignment to this.state
-        // we can have 3 different types of state
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
-    }
+    // our state object which will contain our relevant data
+    // only time we do direct assignment to this.state
+    // we can have 3 different types of state
+    state = { lat: null, errorMessage: ''};
 
     // // this function will be called one time when our component gets rendered onto the screen
     componentDidMount() {
@@ -29,26 +17,14 @@ class App extends React.Component {
 
         // this takes some time so we will have two callbacks -- success and failure
         window.navigator.geolocation.getCurrentPosition(
-            // callback will not run until some point in the future -- until we fetch our position
-            position => {
-                console.log(position);
-                // we call setState to update
-                // setstate gets set when we extend React.Component
-                this.setState({
-                    lat: position.coords.latitude
-                });
-            },
+            // success callback will not run until we fetch our position
+            // setstate gets set when we extend React.Component
+            position => { this.setState({ lat: position.coords.latitude })},
             // handling errors gracefully
             // the user can decline the location request
             // error callback -- rerender our component
-            err => {
-                this.setState({
-                    lat: null,
-                    errorMessage: err.message
-                })
-                console.log(err);
-            }
-        );        
+            err => { this.setState({ errorMessage: err.message})}
+        );
     }
 
     // good for data loading
@@ -59,11 +35,13 @@ class App extends React.Component {
     // need to define render that returns JSX
     // will get called many time
     render() {
-        if (this.state.errorMessage && !this.state.lat) 
+        if (this.state.errorMessage && !this.state.lat)
             return <div>Error: {this.state.errorMessage}</div>
-        
+
+        // pass the latitude to the season component
+        // using the props system
         if (!this.state.errorMessage && this.state.lat)
-            return <div> Latitude: {this.state.lat} </div>;
+            return <SeasonDisplay lat={this.state.lat}/>;
 
         return <div>Loading</div>
     }
